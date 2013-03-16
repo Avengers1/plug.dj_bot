0448e6f5cd5f15b6575202932173afe5ff644750
aITy <aITy132@gmail.com>
2013-03-16 04:23:12 +0100

    k

--- plugbot.js
+++ plugbot.js
@@ -476,6 +476,7 @@ function initAPIListeners() {
                     ret = obj.message.search('/watch');
                     if (ret != -1) {
                         var id_to_watch = "";
+                        console.log(obj.message);
                         if (obj.message.substring(7, 11) == "&#34;") {
                             console.log("First quot found .... ");
                             var ind = indexof("&#34;", 11);
